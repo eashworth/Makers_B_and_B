ea4501c3f00c6c5c87e4b49e942351dbaede137e
Mike <mjwalshe@gmail.com>
2019-09-24 16:46:03 +0100

    Add view database entry, tested with test database

--- app.rb
+++ app.rb
@@ -5,9 +5,15 @@ require 'data_mapper'
 require 'dm-migrations'
 require 'sinatra'
 
-DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'postgres://michael@127.0.0.1/makersbnb')
-DataMapper.auto_migrate!
+if ENV['ENVIRONMENT'] == 'test'
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'postgres://michael@127.0.0.1/makersbnb_test')
+  DataMapper.auto_migrate!
+else
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(:default, 'postgres://michael@127.0.0.1/makersbnb')
+  DataMapper.finalize
+end
 
 class MakersBnb < Sinatra::Base
   set :public_folder, Proc.new { File.join(root, "static") }
